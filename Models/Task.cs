84be13897147b9e533ae8d5be0c6ff411f3dc3bf
kirillgenets <kirill.genets2002@mail.ru>
2020-03-11 22:03:27 +0300

    created Task model + TaskContext + registered this context

---
+++ Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamAppService.Models
+{
+    public class Task
+    {
+        public long ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ISet<int> Executors;
+        public bool IsCompleted { get; set; }
+
+        public void AddExecutor(int id)
+        {
+            Executors.Add(id);
+        }
+
+        public void RemoveExecutor(int id)
+        {
+            Executors.Remove(id);
+        }
+    }
+}
