d2364457fd5b95d475cdb2ad890f4e7f4085f4e9
Kirill Genets <kirill.genets2002@mail.ru>
2020-04-16 21:45:22 +0300

    implemented start version of the service

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,108 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeamAppService.Models;
+
+namespace TeamAppService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly TaskContext _context;
+
+        public TasksController(TaskContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.Task>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Models.Task>> GetTask(long id)
+        {
+            var task = await _context.TodoItems.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // PUT: api/Tasks/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(long id, Models.Task task)
+        {
+            if (id != task.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Models.Task>> PostTask(Models.Task task)
+        {
+            _context.TodoItems.Add(task);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTask), new { id = task.ID }, task);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Models.Task>> DeleteTask(long id)
+        {
+            var task = await _context.TodoItems.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return task;
+        }
+
+        private bool TaskExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.ID == id);
+        }
+    }
+}
